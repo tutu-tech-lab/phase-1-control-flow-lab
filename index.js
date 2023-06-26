e2ad645de1264a4395005ed06d1af7179e894b47
Joy <joy.macharia@student.moringaschool.com>
2023-06-26 19:19:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  
+  if(feet <= 400){
+    return "This one is on me!";
+  } else if(feet <= 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet >= 2500) {
+    return  "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
+console.log(scuberGreetingForFeet(3000));
+  
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === "NYC"? "Ok, sounds good." : "No go."
 }
+console.log(ternaryCheckCity("Pittsburg"));
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(greetings){
   // Write your code here!
+  switch (greetings){
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+      return "Thank you.";
+      default:
+      return "Bye.";
+
+
+     
+
+  }
 }
